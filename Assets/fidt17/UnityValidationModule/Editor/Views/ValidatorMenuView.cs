1e4d9a4ee86dbcfcfcdf024492d13c8100150a59
Vladimir <fidt17@gmail.com>
2023-06-13 22:25:26 +0300

    improve window layout

---
+++ Assets/fidt17/UnityValidationModule/Editor/Views/ValidatorMenuView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace fidt17.UnityValidationModule.Editor.Views
+{
+	internal class ValidatorMenuView
+	{
+		internal class MenuElement
+		{
+			public readonly string Title;
+			public readonly Action OnGUICallback;
+
+			public MenuElement(string title, Action onGUICallback)
+			{
+				Title = title;
+				OnGUICallback = onGUICallback;
+			}
+		}
+
+		private int _activeElementIdx;
+		private readonly List<MenuElement> _elements;
+		private readonly string[] _elementNames;
+
+		public ValidatorMenuView(List<MenuElement> elements, int defaultTabIdx)
+		{
+			_elements = elements;
+			_elementNames = _elements.Select(x => x.Title).ToArray();
+			_activeElementIdx = defaultTabIdx;
+		}
+
+		public void OnGUI()
+		{
+			if (_elements.Count == 0) return;
+
+			try
+			{
+				_activeElementIdx = GUILayout.Toolbar(_activeElementIdx, _elementNames);
+				GUILayout.Space(5);
+				_elements[_activeElementIdx]?.OnGUICallback();
+			}
+			catch (Exception)
+			{
+				// ignore
+			}
+		}
+	}
+}
---
+++ Assets/fidt17/UnityValidationModule/Editor/Views/ValidatorSettingsView.cs
@@ -0,0 +1,78 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace fidt17.UnityValidationModule.Editor.Views
+{
+	internal class ValidatorSettingsView
+	{
+		private readonly ValidatorSettings _settings;
+
+		public ValidatorSettingsView()
+		{
+			_settings = ValidatorSettings.GetSettings();
+		}
+
+		public void OnGUI()
+		{
+			EditorGUILayout.BeginVertical();
+			{
+				EditorGUILayout.Space(3);
+				if (_settings == null)
+				{
+					EditorGUILayout.LabelField("Could not find settings file", new GUIStyle(GUI.skin.label)
+					{
+						fontSize = 12
+					});
+				}
+				else
+				{
+					EditorGUILayout.LabelField("Build Settings", new GUIStyle(GUI.skin.label)
+					{
+						fontSize = 13
+					});
+			
+					EditorGUILayout.Space(5);
+					EditorGUILayout.BeginHorizontal();
+					{
+						EditorGUILayout.LabelField("Run Validator on build", new GUIStyle(GUI.skin.label)
+						{
+							fontSize = 12,
+						});
+						var runValidatorOnBuild = EditorGUILayout.Toggle(_settings.RunValidatorOnBuild, new GUIStyle(GUI.skin.toggle)
+						{
+							alignment = TextAnchor.MiddleLeft
+						});
+						if (runValidatorOnBuild != _settings.RunValidatorOnBuild)
+						{
+							_settings.RunValidatorOnBuild = runValidatorOnBuild;
+							EditorUtility.SetDirty(_settings);
+							AssetDatabase.SaveAssets();
+						}
+			
+					}
+					EditorGUILayout.EndHorizontal();
+				
+					EditorGUILayout.BeginHorizontal();
+					{
+						EditorGUILayout.LabelField("Stop build on Validator failure", new GUIStyle(GUI.skin.label)
+						{
+							fontSize = 12,
+						});
+						var stopBuildOnValidatorFailure = EditorGUILayout.Toggle(_settings.StopBuildIfValidatorFails, new GUIStyle(GUI.skin.toggle)
+						{
+							alignment = TextAnchor.MiddleLeft
+						});
+						if (stopBuildOnValidatorFailure != _settings.StopBuildIfValidatorFails)
+						{
+							_settings.StopBuildIfValidatorFails = stopBuildOnValidatorFailure;
+							EditorUtility.SetDirty(_settings);
+							AssetDatabase.SaveAssets();
+						}
+					}
+					EditorGUILayout.EndHorizontal();
+				}
+			}
+			EditorGUILayout.EndVertical();
+		}
+	}
+}
